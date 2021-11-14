c5b9a4c48694ac3010f0f2465a7fadd3619eba49
asb1678 <83837340+asb1678@users.noreply.github.com>
2021-11-14 16:13:20 -0500

    Add files via upload

---
+++ src/models/Keyword.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Keyword{
+   private int keywordID;
+   private String text;
+   
+   public int getKeywordID() {return keywordID;}
+   public void setKeywordID(int keywordID) {this.keywordID = keywordID;}
+   
+   public String getText() {return text;} 
+   public void setText(String text) {this.text = text;}
+   
+   public StudentKeyword(int keywordID, String text) {
+      this.keywordID = keywordID;
+      this.text = text;
+   }
+   
+   public Keyword() { }
+}
