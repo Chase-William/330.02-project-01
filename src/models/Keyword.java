2d905269b028dd0e624347a8432d09fbe22c6c23
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2021-11-15 13:22:12 -0500

    corrected name of the constructor

--- src/models/Keyword.java
+++ src/models/Keyword.java
@@ -3,17 +3,17 @@ package models;
 public class Keyword{
    private int keywordID;
    private String text;
-   
+
    public int getKeywordID() {return keywordID;}
    public void setKeywordID(int keywordID) {this.keywordID = keywordID;}
-   
-   public String getText() {return text;} 
+
+   public String getText() {return text;}
    public void setText(String text) {this.text = text;}
-   
-   public StudentKeyword(int keywordID, String text) {
+
+   public Keyword(int keywordID, String text) {
       this.keywordID = keywordID;
       this.text = text;
    }
-   
+
    public Keyword() { }
 }
