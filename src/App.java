5ada4c37f74a2213429a1d0352ad33581b9ec7d6
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2021-11-17 13:58:43 -0500

    fixed regex condition + format

--- src/App.java
+++ src/App.java
@@ -10,137 +10,146 @@ public class App {
    final String studentMenu = "\n\033[0;36mWhat would like you to do?\n\033[0;35m1. Search professor(s) via interests\n2. Log out\n3. Exit the program";
    final String professorMenu = "\n\033[0;36mWhat would you like to do?\n\033[0;35m1. Search and subscribe to abstracts\n2. View subscribed abstracts\n3. Log out\n4. Exit the program";
 
-   public App(DataLayer data, Scanner scan){
+   public App(DataLayer data, Scanner scan) {
       this.data = data;
       this.scan = scan;
    }
 
-   public void run(){
-      if(data.connect("qn182535")){
+   public void run() {
+      if (data.connect("qn182535")) {
          data.loadAbstracts(new File());
          System.out.println("Welcome to FacultyResearch!");
          menu("login", "[1-3]", logMenu);
       }
    }
 
-   public void logIn(String user){
-      System.out.println("\n\033[0;34mYou are logging in as a "+user+"\n\nPlease enter your email and password\033[0m");
+   public void logIn(String user) {
+      System.out
+            .println("\n\033[0;34mYou are logging in as a " + user + "\n\nPlease enter your email and password\033[0m");
       System.out.print("Email: ");
       String email = scan.nextLine();
 
       boolean isSuccess = false;
 
-      if(user.equals("professor")){
+      if (user.equals("professor")) {
          System.out.print("Password: ");
          String password = scan.nextLine();
          isSuccess = data.checkLog(email, password);
-      }
-      else if(user.equals("student"))
+      } else if (user.equals("student"))
          isSuccess = data.checkLog(email);
 
-      if(isSuccess){
-         if(data.user != null)
-            System.out.println("\n\033[0;32mSuccessfully logged in.\nWelcome, "+data.user.getFirstName()+" "+data.user.getLastName()+"!");
-         if(user.equals("professor"))
+      if (isSuccess) {
+         if (data.user != null)
+            System.out.println("\n\033[0;32mSuccessfully logged in.\nWelcome, " + data.user.getFirstName() + " "
+                  + data.user.getLastName() + "!");
+         if (user.equals("professor"))
             menu("professor", "[1-4]", professorMenu);
-         else if(user.equals("student"))
+         else if (user.equals("student"))
             menu("student", "[1-3]", studentMenu);
-      }
-      else
+      } else
          System.out.println("\n\033[0;31mThe email or password you entered is either incorrect or not in the system.");
    }
 
-   public void logOut(){
+   public void logOut() {
       System.out.println("\n\033[0;32mSuccessfully logged out.");
       data.user = null;
       menu("login", "[1-3]", logMenu);
    }
 
-   public void menu(String typeMenu, String regexCondition, String menuString){
-      while(true){
+   public void menu(String typeMenu, String regexCondition, String menuString) {
+      while (true) {
          int input = getInput(regexCondition, menuString);
-         if(input > 0){
-            switch(typeMenu){
-               case "login" -> logChoice(input);
-               case "professor" -> professorChoice(input);
-               case "student" -> studentChoice(input);
+         if (input > 0) {
+            switch (typeMenu) {
+            case "login" -> logChoice(input);
+            case "professor" -> professorChoice(input);
+            case "student" -> studentChoice(input);
             }
-         }
-         else
+         } else
             System.out.println("\n\033[0;31mYou have entered invalid input, please try again");
       }
    }
 
-   public void logChoice(int input){
-      switch(input){
-         case 1 -> logIn("professor");
-         case 2 -> logIn("student");
-         case 3 -> exit();
+   public void logChoice(int input) {
+      switch (input) {
+      case 1 -> logIn("professor");
+      case 2 -> logIn("student");
+      case 3 -> exit();
       }
    }
 
-   public void professorChoice(int input){
-      switch(input){
-         case 1 -> searchAbstracts();
-         case 2 -> viewAbstracts();
-         case 3 -> logOut();
-         case 4 -> exit();
+   public void professorChoice(int input) {
+      switch (input) {
+      case 1 -> searchAbstracts();
+      case 2 -> viewAbstracts();
+      case 3 -> logOut();
+      case 4 -> exit();
       }
    }
 
-   public void studentChoice(int input){
-      switch(input){
-         case 1 -> searchProfessors();
-         case 2 -> logOut();
-         case 3 -> exit();
+   public void studentChoice(int input) {
+      switch (input) {
+      case 1 -> searchProfessors();
+      case 2 -> logOut();
+      case 3 -> exit();
       }
    }
 
-   public int getInput(String regexCondition, String menuString){
+   public int getInput(String regexCondition, String menuString) {
       System.out.println(menuString);
       System.out.print("\033[0;36mOption:\033[0m ");
       String input = scan.nextLine();
-      if(input.matches(regexCondition))
+      if (input.matches(regexCondition))
          return Integer.parseInt(input);
       return 0;
    }
 
-   public void exit(){
+   public void exit() {
       data.close();
       System.exit(0);
    }
 
-   public void searchAbstracts(){
-      if(data.canSearchAbstracts()){
-         while(true){
+   public void searchAbstracts() {
+      if (data.canSearchAbstracts()) {
+         while (true) {
             int count = 1;
-            System.out.println("\n\033[0;36mEnter option to view abstract's detail\n\033[0;35m"+count+". Cancel searching");
-            for(Abstract abstracts : data.abstracts){
+            System.out.println(
+                  "\n\033[0;36mEnter option to view abstract's detail\n\033[0;35m" + count + ". Cancel searching");
+            for (Abstract a : data.abstracts) {
                count++;
-               System.out.println(count +". "+(abstracts.getTitle()));
+               System.out.println(count + ". " + (a.getTitle()));
             }
             System.out.print("\033[0;36mOption:\033[0m ");
             String input = scan.nextLine();
-            if(input.matches("[1-"+count+"]"))
-               if(!input.equals("1"))
-                  System.out.println(data.abstracts.get(Integer.parseInt(input)-2).toString());
+            if (tryParseInt(input)) {
+               if (Integer.parseInt(input) > 1 && Integer.parseInt(input) <= count)
+                  System.out.println(data.abstracts.get(Integer.parseInt(input) - 2).toString());
                else
                   break;
-            else
+            } else
                System.out.println("\n\033[0;31mYou have entered invalid input");
          }
       }
    }
 
-   public void viewAbstracts(){
-      System.out.println("\nthis function [viewAbstracts] is out of service");
-      //data.viewAbstracts();
+   public boolean tryParseInt(String value) {
+      try {
+         Integer.parseInt(value);
+         return true;
+      } catch (NumberFormatException e) {
+         return false;
+      }
    }
 
-   public void searchProfessors(){
+   public void viewAbstracts() {
+      System.out.println("\nthis function [viewAbstracts] is out of service");
+      // data.viewAbstracts();
+   }
+
+   public void searchProfessors() {
       System.out.println("\nthis function [searchProfessors] is out of service");
-      //List<String> keywordList = new LinkedList();
-      //data.searchProfessor(keywordList);
+      // List<String> keywordList = new LinkedList();
+      // data.searchProfessor(keywordList);
    }
 
    public static void main(String[] args) throws Exception {
@@ -150,7 +159,8 @@ public class App {
       // Scanner scan = new Scanner(System.in);
       // DataLayer layer = new DataLayer();
       // layer.connect("Csharp304360283!");
-      // //layer.insertProfessor(new Faculty("Chase", "Roth", "cxr6988@g.rit.edu", 10, 20));
+      // //layer.insertProfessor(new Faculty("Chase", "Roth", "cxr6988@g.rit.edu", 10,
+      // 20));
 
       // // Declare variable
       // int option;
@@ -170,50 +180,55 @@ public class App {
       // option = scan.nextInt();
 
       // /* Option 1 - Insert a Faculty
-      //  * Basic informations of the Faculty
-      //  * Answers the Abstract file name
-      //  */
+      // * Basic informations of the Faculty
+      // * Answers the Abstract file name
+      // */
       // if (option == 1){
-      //    System.out.print("Please type first name: ");
-      //    firstName = scan.nextLine();
-      //    System.out.print("Please type last name: ");
-      //    lastName = scan.nextLine();
-      //    System.out.print("Please type email: ");
-      //    email = scan.nextLine();
-      //    System.out.print("Please type Build Number: ");
-      //    buildNumber = scan.nextInt();
-      //    System.out.print("Please type Office Number: ");
-      //    officeNumber = scan.nextInt();
-      //    layer.insertProfessor(new Faculty(firstName, lastName, email, buildNumber, officeNumber));
+      // System.out.print("Please type first name: ");
+      // firstName = scan.nextLine();
+      // System.out.print("Please type last name: ");
+      // lastName = scan.nextLine();
+      // System.out.print("Please type email: ");
+      // email = scan.nextLine();
+      // System.out.print("Please type Build Number: ");
+      // buildNumber = scan.nextInt();
+      // System.out.print("Please type Office Number: ");
+      // officeNumber = scan.nextInt();
+      // layer.insertProfessor(new Faculty(firstName, lastName, email, buildNumber,
+      // officeNumber));
       // }
       // /* Option 2 - Insert a Student
-      //  * Basic informations of the Student
-      //  * Answers the number of topics s/he would like to research
-      //  */
+      // * Basic informations of the Student
+      // * Answers the number of topics s/he would like to research
+      // */
       // else if (option == 2){
-      //    System.out.print("Please type first name: ");
-      //    firstName = scan.nextLine();
-      //    System.out.print("Please type last name: ");
-      //    lastName = scan.nextLine();
-      //    System.out.print("Please type email: ");
-      //    email = scan.nextLine();
-      //    //layer.insertStudent(new Student(firstName, lastName, email));
+      // System.out.print("Please type first name: ");
+      // firstName = scan.nextLine();
+      // System.out.print("Please type last name: ");
+      // lastName = scan.nextLine();
+      // System.out.print("Please type email: ");
+      // email = scan.nextLine();
+      // //layer.insertStudent(new Student(firstName, lastName, email));
       // }
       // /* Option 3 - Search Abstracts
-      //  * Asks the student ID
-      //  * Display a list of faculty  sorted by the most matching Abstracts to least matching Abstracts
-      //  * Asks student which faculty would s/he like to meet and then show the Faculty information
-      //  */
+      // * Asks the student ID
+      // * Display a list of faculty sorted by the most matching Abstracts to least
+      // matching Abstracts
+      // * Asks student which faculty would s/he like to meet and then show the
+      // Faculty information
+      // */
       // else if (option == 3){
-      //    System.out.print("Please type 1-3 words of topic that you want to research: ");
-      //    words = scan.nextLine();
+      // System.out.print("Please type 1-3 words of topic that you want to research:
+      // ");
+      // words = scan.nextLine();
       // }
       // //else if (option == 4){
-      //    //System.out.print("Please type 1-3 words of topic that you want to research: ");
-      //    //words = scan.nextLine();
+      // //System.out.print("Please type 1-3 words of topic that you want to research:
+      // ");
+      // //words = scan.nextLine();
       // //}
       // else {
-      //    System.out.println("Have a nice day!");
+      // System.out.println("Have a nice day!");
       // }
    }
 }
