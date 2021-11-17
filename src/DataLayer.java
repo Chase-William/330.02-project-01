35f0283a357cd73c985266192136a849b0bac00b
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2021-11-17 12:11:45 -0500

    added a function for loadAbstract that retrieves abstract data

--- src/DataLayer.java
+++ src/DataLayer.java
@@ -1,32 +1,67 @@
 import java.sql.*;
 import java.util.*;
 import models.*;
+import services.File;
 
 public class DataLayer {
 
   private Connection conn;
-  //public User user;
+  public User user;
+  public List<Abstract> abstracts;
   /* JDBC Type 4 Driver */
   static final String DEFAULT_DRIVER = "com.mysql.cj.jdbc.Driver";
   static final String DATABASE_NAME = "facultyresearch";
 
-  PreparedStatement stmt;
-  ResultSet result;
+  private Statement stmt;
+  private ResultSet rs;
 
   public boolean checkLog(String email, String password){
-    String sql = "SELECT * FROM user JOIN faculty USING(UserID) WHERE Email = '"+email+"' AND Password = '"+password+"';";
-    // return true if result.count is more than 0
-    return true;
+    // String sql = "SELECT * FROM user JOIN faculty USING(UserID) WHERE UserType = 0 AND Email = '"+email+"' AND Password = '"+password+"';";
+    String sql = "SELECT * FROM user JOIN faculty USING(UserID) WHERE UserType = 0 AND Email = 'lw533@g.rit.edu' AND Password = 's1v$c86gjil';";
+    return executeLog(sql, "professor");
   }
 
   public boolean checkLog(String email){
     String sql = "SELECT * FROM user JOIN student USING(UserID) WHERE Email = '"+email+"';";
-    // return true if result.count is more than 0
-    return true;
+    return executeLog(sql, "student");
   }
 
-  public int searchAbstracts(){
-    return 0;
+  public boolean executeLog(String sqlStatement, String userType){
+    try {
+      conn.setAutoCommit(false);
+      stmt = conn.createStatement();
+      rs = stmt.executeQuery(sqlStatement);
+      while(rs.next()){
+        if(userType.equals("professor"))
+          user = new Faculty(rs.getInt("UserID"), rs.getString("FirstName"), rs.getString("LastName"), rs.getString("Email"), rs.getInt("BuildingNumber"), rs.getInt("OfficeNumber"));
+        else if(userType.equals("student"))
+          user = new Student(rs.getInt("UserID"), rs.getString("FirstName"), rs.getString("LastName"), rs.getString("Email"));
+        break;
+      }
+      conn.setAutoCommit(true);
+      if(rs.getRow() == 1)
+        return true;
+      else
+        return false;
+    }
+    catch (Exception ex) {
+      System.err.println(ex.getLocalizedMessage());
+      try {
+        conn.rollback();
+        conn.setAutoCommit(true);
+      }
+      catch (SQLException ex2) {
+        ex2.printStackTrace();
+      }
+      return false;
+    }
+  }
+
+  public boolean canSearchAbstracts(){
+    if(abstracts.size() > 0)
+      return true;
+    else
+      return false;
   }
 
   public int viewAbstracts(){
@@ -37,74 +72,30 @@ public class DataLayer {
     return 0;
   }
 
-  /**
-   *
-   */
-  // public void insertProfessor(Faculty faculty) {
-  //   try {
-  //     conn.setAutoCommit(false);
-  //     // Insert backing user
-  //     stmt = conn.prepareStatement("INSERT INTO User (FirstName, LastName, Email, UserType) VALUES (?, ?, ?, ?)");
-  //     stmt.setString(1, faculty.getFirstName());
-  //     stmt.setString(2, faculty.getLastName());
-  //     stmt.setString(3, faculty.getEmail());
-  //     stmt.setInt(4, User.UserType.FACULTY.ordinal());
-  //     int rows = stmt.executeUpdate(SQL);
-  //     result.next();
-  //     int id = result.getInt(1);
-  //     // Insert faculty
-  //     stmt = conn.prepareStatement("INSERT INTO professor VALUES(?, ?, ?, ?, ?)");
-  //     stmt.setInt(1, id);
-  //     stmt.setInt(2, faculty.getBuildingNumber());
-  //     stmt.setInt(3, faculty.getOfficeNumber());
-  //     stmt.setString(4, faculty.getPassword());
-
-  //     conn.setAutoCommit(true);
-  //   }
-  //   catch (Exception ex) {
-  //     System.err.println("Failed to insert User->Faculty.");
-  //     System.err.println(ex.getLocalizedMessage());
-  //     try {
-  //       conn.rollback();
-  //       conn.setAutoCommit(true);
-  //     }
-  //     catch (SQLException ex2) {
-  //       ex2.printStackTrace();
-  //     }
-  //   }
-  // }
-
-  /**
-   *
-   */
-  // public void insertStudent(Student student) {
-  //   try {
-  //     conn.setAutoCommit(false);
-  //     // Insert backing user
-  //     stmt = conn.prepareStatement("INSERT INTO User (FirstName, LastName, Email, UserType) VALUES (?, ?, ?, ?)");
-  //     stmt.setString(1, student.getFirstName());
-  //     stmt.setString(2, student.getLastName());
-  //     stmt.setString(3, student.getEmail());
-  //     stmt.setInt(4, User.UserType.STUDENT.ordinal());
-  //     int rows = stmt.executeUpdate(SQL);
-  //     result.next();
-  //     int id = result.getInt(1);
-  //     // Insert student
-
-  //     conn.setAutoCommit(true);
-  //   }
-  //   catch (Exception ex) {
-  //     System.err.println("Failed to insert User->Student.");
-  //     System.err.println(ex.getLocalizedMessage());
-  //     try {
-  //       conn.rollback();
-  //       conn.setAutoCommit(true);
-  //     }
-  //     catch (SQLException ex2) {
-  //       ex2.printStackTrace();
-  //     }
-  //   }
-  // }
+  public void loadAbstracts(File file){
+    abstracts = new LinkedList<Abstract>();
+    try {
+      conn.setAutoCommit(false);
+      stmt = conn.createStatement();
+      rs = stmt.executeQuery("SELECT * FROM abstract;");
+      String description = new String();
+      while(rs.next()){
+        description = file.read(rs.getString("FileName"));
+        abstracts.add(new Abstract(rs.getInt("AbstractID"),rs.getString("Title"),description));
+      }
+      conn.setAutoCommit(true);
+    }
+    catch (Exception ex) {
+      System.err.println(ex.getLocalizedMessage());
+      try {
+        conn.rollback();
+        conn.setAutoCommit(true);
+      }
+      catch (SQLException ex2) {
+        ex2.printStackTrace();
+      }
+    }
+  }
 
   /**
    * Connect the database using the cached creds.
