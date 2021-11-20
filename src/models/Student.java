78e86b78751cae6ef49c0f5e4f465aeb9f58ba10
Quoc Phi Nhan <46696373+qwokwok@users.noreply.github.com>
2021-11-19 19:10:27 -0500

    moved to 'zip-this folder'

--- src/models/Student.java
+++
@@ -1,9 +0,0 @@
-package models;
-
-public class Student extends User {
-  public Student(int userID, String firstName, String lastName, String email) {
-    super(userID, firstName, lastName, email);
-  }
-
-  public Student() { }
-}
